3d0a4b8c7628b277208625c12866d6e07ba1eec7
Paweł Adamowicz <adamowicz.p.zseil@gmail.com>
2019-02-03 21:57:13 +0100

    Add contact us feature file and feature steps

---
+++ src/test/java/io/cucumber/contactus/ContactUsSteps.java
@@ -0,0 +1,9 @@
+package io.cucumber.loginpage;
+
+import cucumber.api.java.en.And;
+
+public class ContactUsSteps {
+    @And("^User enters first name$")
+    public void userEntersFirstName() {
+    }
+}
