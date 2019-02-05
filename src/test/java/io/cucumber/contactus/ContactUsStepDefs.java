f578162522f2f43ee532d8abb4a11efcf503354d
Paweł Adamowicz <adamowicz.p.zseil@gmail.com>
2019-02-05 21:43:29 +0100

    add missing files

---
+++ src/test/java/io/cucumber/contactus/ContactUsStepDefs.java
@@ -0,0 +1,9 @@
+package io.cucumber.contactus;
+
+import cucumber.api.java.en.Given;
+
+public class ContactUsStepDefs {
+    @Given("^I access webdriveruniversity$")
+    public void iAccessWebdriveruniversity() {
+    }
+}
